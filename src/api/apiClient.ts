03ec759d2a89fc6e03239d2e19deba10eb7d8fb2
Ravel Becker <ravel.beckermhd@gmail.com>
2025-06-03 21:50:57 -0300

    render

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://brain-ag-api-cqe8.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
